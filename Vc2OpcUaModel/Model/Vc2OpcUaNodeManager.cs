28fb9af4716dd82461b2c50b152c0c8eb055ae2c
Alvaro Capellan <capellan.alvaro@gmail.com>
2018-03-08 10:05:28 +0100

    Added opcua .NET stack model code

---
+++ Vc2OpcUaModel/Model/Vc2OpcUaNodeManager.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+using Opc.Ua;
+using Opc.Ua.Sample;
+using System.Reflection;
+
+namespace Vc2OpcUa
+{
+    /// <summary>
+    /// A node manager the diagnostic information exposed by the server.
+    /// </summary>
+    public class Vc2OpcUaNodeManager : SampleNodeManager
+    {
+        #region Constructors
+        /// <summary>
+        /// Initializes the node manager.
+        /// </summary>
+        public Vc2OpcUaNodeManager(
+            Opc.Ua.Server.IServerInternal server, 
+            ApplicationConfiguration configuration)
+        :
+            base(server)
+        {
+            List<string> namespaceUris = new List<string>();
+            namespaceUris.Add(Namespaces.Vc2OpcUa);
+            namespaceUris.Add(Namespaces.Vc2OpcUa + "/Instance");
+            NamespaceUris = namespaceUris;
+
+            m_typeNamespaceIndex = Server.NamespaceUris.GetIndexOrAppend(namespaceUris[0]);
+            m_namespaceIndex = Server.NamespaceUris.GetIndexOrAppend(namespaceUris[1]);
+
+            m_lastUsedId = 0;
+        }
+        #endregion
+
+
+        /// <summary>
+        /// Loads a node set from a file or resource and addes them to the set of predefined nodes.
+        /// </summary>
+        protected override NodeStateCollection LoadPredefinedNodes(ISystemContext context)
+        {
+            NodeStateCollection predefinedNodes = new NodeStateCollection();
+            predefinedNodes.LoadFromBinaryResource(context, "Opc.Ua.Sample.Vc2OpcUa.PredefinedNodes.uanodes", this.GetType().GetTypeInfo().Assembly, true);
+            return predefinedNodes;
+        }
+
+        #region Private Fields
+        private ushort m_namespaceIndex;
+        private ushort m_typeNamespaceIndex;
+        private long m_lastUsedId;
+        #endregion
+
+    }
+
+}
---
+++ vc2opcua/opcua-server/Server.cs
@@ -0,0 +1,259 @@
+﻿/* Copyright (c) 1996-2016, OPC Foundation. All rights reserved.
+   The source code in this file is covered under a dual-license scenario:
+     - RCL: for OPC Foundation members in good-standing
+     - GPL V2: everybody else
+   RCL license terms accompanied with this source code. See http://opcfoundation.org/License/RCL/1.00/
+   GNU General Public License as published by the Free Software Foundation;
+   version 2 of the License are accompanied with this source code. See http://opcfoundation.org/License/GPLv2
+   This source code is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Opc.Ua;
+using Opc.Ua.Configuration;
+using Opc.Ua.Server;
+using Vc2OpcUaServer;
+
+namespace NetCoreConsoleServer
+{
+    public class ApplicationMessageDlg : IApplicationMessageDlg
+    {
+        private string message = string.Empty;
+        private bool ask = false;
+
+        public override void Message(string text, bool ask)
+        {
+            this.message = text;
+            this.ask = ask;
+        }
+
+        public override async Task<bool> ShowAsync()
+        {
+            if (ask)
+            {
+                message += " (y/n, default y): ";
+                Console.Write(message);
+            }
+            else
+            {
+                Console.WriteLine(message);
+            }
+            if (ask)
+            {
+                try
+                {
+                    ConsoleKeyInfo result = Console.ReadKey();
+                    Console.WriteLine();
+                    return await Task.FromResult((result.KeyChar == 'y') || (result.KeyChar == 'Y') || (result.KeyChar == '\r'));
+                }
+                catch
+                {
+                    // intentionally fall through
+                }
+            }
+            return await Task.FromResult(true);
+        }
+    }
+
+    public enum ExitCode : int
+    {
+        Ok = 0,
+        ErrorServerNotStarted = 0x80,
+        ErrorServerRunning = 0x81,
+        ErrorServerException = 0x82,
+        ErrorInvalidCommandLine = 0x100
+    };
+
+    public class Program
+    {
+
+        public static int Main(string[] args)
+        {
+            // command line options
+            bool showHelp = false;
+            int stopTimeout = 0;
+            bool autoAccept = false;
+
+            // Start MyServer
+            MyServer server = new MyServer(autoAccept, stopTimeout);
+            server.Run();
+
+            return (int)MyServer.ExitCode;
+        }
+    }
+
+    public class MyServer
+    {
+        UaServer server;
+        Task status;
+        DateTime lastEventTime;
+        int serverRunTime = Timeout.Infinite;
+        static bool autoAccept = false;
+        static ExitCode exitCode;
+
+        public MyServer(bool _autoAccept, int _stopTimeout)
+        {
+            autoAccept = _autoAccept;
+            serverRunTime = _stopTimeout == 0 ? Timeout.Infinite : _stopTimeout * 1000;
+        }
+
+        public void Run()
+        {
+
+            try
+            {
+                exitCode = ExitCode.ErrorServerNotStarted;
+                // Start the server
+                ConsoleServer().Wait();
+                Console.WriteLine("[NapServer-Program.cs] Server started. Press Ctrl-C to exit...");
+                exitCode = ExitCode.ErrorServerRunning;
+            }
+            catch (Exception ex)
+            {
+                Utils.Trace("ServiceResultException:" + ex.Message);
+                Console.WriteLine("Exception: {0}", ex.Message);
+                exitCode = ExitCode.ErrorServerException;
+                return;
+            }
+
+            ManualResetEvent quitEvent = new ManualResetEvent(false);
+            try
+            {
+                Console.CancelKeyPress += (sender, eArgs) => {
+                    quitEvent.Set();
+                    eArgs.Cancel = true;
+                };
+            }
+            catch
+            {
+            }
+
+            // wait for timeout or Ctrl-C
+            quitEvent.WaitOne(serverRunTime);
+
+            if (server != null)
+            {
+                Console.WriteLine("Server stopped. Waiting for exit...");
+
+                using (UaServer _server = server)
+                {
+                    // Stop status thread
+                    server = null;
+                    status.Wait();
+                    // Stop server and dispose
+                    _server.Stop();
+                }
+            }
+
+            exitCode = ExitCode.Ok;
+        }
+
+        public static ExitCode ExitCode { get => exitCode; }
+
+        private static void CertificateValidator_CertificateValidation(CertificateValidator validator, CertificateValidationEventArgs e)
+        {
+            if (e.Error.StatusCode == StatusCodes.BadCertificateUntrusted)
+            {
+                e.Accept = autoAccept;
+                if (autoAccept)
+                {
+                    Console.WriteLine("Accepted Certificate: {0}", e.Certificate.Subject);
+                }
+                else
+                {
+                    Console.WriteLine("Rejected Certificate: {0}", e.Certificate.Subject);
+                }
+            }
+        }
+
+        private async Task ConsoleServer()
+        {
+            ApplicationInstance.MessageDlg = new ApplicationMessageDlg();
+            ApplicationInstance application = new ApplicationInstance();
+
+            application.ApplicationName = "UA Core Server";
+            application.ApplicationType = ApplicationType.Server;
+            application.ConfigSectionName = Utils.IsRunningOnMono() ? "Opc.Ua.MonoSampleServer" : "Opc.Ua.SampleServer";
+
+            // load the application configuration.
+            ApplicationConfiguration config = await application.LoadApplicationConfiguration(false);
+
+            // check the application certificate.
+            bool haveAppCertificate = await application.CheckApplicationInstanceCertificate(false, 0);
+            if (!haveAppCertificate)
+            {
+                throw new Exception("Application instance certificate invalid!");
+            }
+
+            if (!config.SecurityConfiguration.AutoAcceptUntrustedCertificates)
+            {
+                config.CertificateValidator.CertificateValidation += new CertificateValidationEventHandler(CertificateValidator_CertificateValidation);
+            }
+
+            Console.WriteLine("[NapServer-Program.cs] Start the server");
+            // start the server.
+            server = new UaServer();
+            await application.Start(server);
+
+            // start the status thread
+            status = Task.Run(new Action(StatusThread));
+
+            // print notification on session events
+            server.CurrentInstance.SessionManager.SessionActivated += EventStatus;
+            server.CurrentInstance.SessionManager.SessionClosing += EventStatus;
+            server.CurrentInstance.SessionManager.SessionCreated += EventStatus;
+
+        }
+
+        private void EventStatus(Session session, SessionEventReason reason)
+        {
+            lastEventTime = DateTime.UtcNow;
+            PrintSessionStatus(session, reason.ToString());
+        }
+
+        void PrintSessionStatus(Session session, string reason, bool lastContact = false)
+        {
+            lock (session.DiagnosticsLock)
+            {
+                string item = String.Format("{0,9}:{1,20}:", reason, session.SessionDiagnostics.SessionName);
+                if (lastContact)
+                {
+                    item += String.Format("Last Event:{0:HH:mm:ss}", session.SessionDiagnostics.ClientLastContactTime.ToLocalTime());
+                }
+                else
+                {
+                    if (session.Identity != null)
+                    {
+                        item += String.Format(":{0,20}", session.Identity.DisplayName);
+                    }
+                    item += String.Format(":{0}", session.Id);
+                }
+                Console.WriteLine(item);
+            }
+        }
+
+        private async void StatusThread()
+        {
+            while (server != null)
+            {
+                if (DateTime.UtcNow - lastEventTime > TimeSpan.FromMilliseconds(6000))
+                {
+                    IList<Session> sessions = server.CurrentInstance.SessionManager.GetSessions();
+                    for (int ii = 0; ii < sessions.Count; ii++)
+                    {
+                        Session session = sessions[ii];
+                        PrintSessionStatus(session, "-Status-", true);
+                    }
+                    lastEventTime = DateTime.UtcNow;
+                }
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
